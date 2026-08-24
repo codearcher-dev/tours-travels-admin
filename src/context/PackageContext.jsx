fa2a8994c2f8eedd8229782f96a5af74eba5e9ac
Rahul Vishwakarma <rahulrjp16@gmail.com>
2026-08-24 14:52:26 +0530

    Package context created

---
+++ src/context/PackageContext.jsx
@@ -0,0 +1,38 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { getAllPackages } from "../services/packages.services";
+import { getDestinations } from "../services/destination.services";
+
+const PackageContext = createContext();
+
+export const PackageProvider = ({ children }) => {
+    const [packages, setPackages] = useState([]);
+    const [destinations, setDestinations] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    const fetch = async () => {
+        setLoading(true);
+        setError(null);
+
+        try {
+            const packagesData = await getAllPackages();
+            const destinationData = await getDestinations();
+            console.log(packages);
+
+            setPackages(packagesData.packages);
+            setDestinations(destinationData.destinations);
+        } catch (error) {
+            setError(error.response?.data?.message || "Failed to fetch packages");
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        fetch();
+    }, []);
+
+    return <PackageContext.Provider value={{ packages, destinations, loading, error, retry: fetch }}>{children}</PackageContext.Provider>;
+};
+
+export const usePackages = () => useContext(PackageContext);
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { PackageProvider } from "./context/PackageContext.jsx";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+    <StrictMode>
+        <BrowserRouter>
+            <PackageProvider>
+                <App />
+            </PackageProvider>
+        </BrowserRouter>
+    </StrictMode>,
+);
